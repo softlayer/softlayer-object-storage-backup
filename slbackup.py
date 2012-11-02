896aaf5a9652ccc3d8a81310df0ab7132e208ad0
Kevin Landreth <klandreth@softlayer.com>
2012-11-02 08:53:15 -0500

    Resolved issue #12

--- slbackup.py
+++ slbackup.py
@@ -465,7 +465,7 @@ def upload_files(_container, jobs):
             chunk_upload(obj, _file)
             l.warn("Finished file %s ", obj.name)
         except Exception, e:
-            l.error("Failed to upload %s, requeueing", _file)
+            l.error("Failed to upload %s, requeueing. Error: %s", _file, e)
             jobs.put((_file, target,))
             # in case we got disconnected, reset the container
             container = get_container(app, name=_container)
