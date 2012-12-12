90ffb522779e171a7622ee29aa7ef5b9f35aef43
Kevin Landreth <klandreth@softlayer.com>
2012-12-12 08:29:53 -0600

    Added setup.py for installation and pypi

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+from slbackup import __version__
+
+setup(name='slbackup',
+      version=__version__,
+      description='Backup utility for SWIFT object storage',
+      author='Kevin Landreth',
+      author_email='klandreth@softlayer.com',
+      url="https://github.com/softlayer/softlayer-object-storage-backup",
+      license='MIT',
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['softlayer-object-storage>=0.4.4'],
+      scripts=['slbackup.py'],
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Programming Language :: Python',
+        'Operating System :: Linux',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        ],
+      )
